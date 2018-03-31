46f4cfe25759c54cc427e4c6bb2a2df282fb1b7f
Joe H <josephhavlik@hotmail.com>
2018-03-30 20:09:29 -0500

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~/workspace
-tar -czf brewCityRentals.tar.gz brewCityRentals/
-scp brewCityRentals.tar.gz jrhavlik@uwmsois.com:. 
-ssh jrhavlik@uwmsois.com "tar -xzf brewCityRentals.tar.gz && rm brewCityRentals.tar.gz"
-rm brewCityRentals.tar.gz
